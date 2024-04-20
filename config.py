8991e312144dc0004ec56201a14ed7f64ecb4cae
Naveen Rajendran <navin.rkn@gmail.com>
2024-04-20 16:30:27 +0530

    Comminit Initial CRM App Stack

---
+++ config.py
@@ -0,0 +1,24 @@
+# Import community modules.
+import os
+import json
+
+# Get App configuration.
+def app_conf():
+  file = open('config/app.conf','r')
+  conf = file.read()
+  conf = json.loads(conf)
+  file.close()
+  if 'STATIC_ENDPOINT' in os.environ:
+    conf['cdn']['static']['endpoint'] = os.environ['STATIC_ENDPOINT']
+  return conf
+
+# Get Redis configuration.
+def redis_conf():
+  file = open('config/redis.conf','r')
+  conf = file.read()
+  conf = json.loads(conf)
+  file.close()
+  return conf
+
+app_conf = app_conf()
+redis_conf = redis_conf()
